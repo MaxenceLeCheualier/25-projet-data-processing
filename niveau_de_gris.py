e63e4b6a3fb89022932a803ceb1034145d42a432
Lukas Fermaut <lukas.fermaut@etu.minesparis.psl.eu>
2026-06-29 10:20:26 +0200

    Image convert

--- niveau_de_gris.py
+++ niveau_de_gris.py
@@ -1,23 +1,32 @@
 import numpy as np
 from PIL import Image 
 
-img = Image.open(r"projet_mines-Paris-data-processing_2026\donnes_MEB-EDS\export_tif\Laitier1_Al-K.tif")
-
 def convert_gray(image):
     image_array = np.array(image)
     R=image_array[:,:,0]
     G=image_array[:,:,1]
     B=image_array[:,:,2]
-    Gray =  (0.2126*R + 0.7152*G + 0.0722*B).astype(np.uint8) #convention 709
+    Gray =  np.round(0.2126*R + 0.7152*G + 0.0722*B).astype(np.uint8) #convention 709
     image_gray = Image.fromarray(Gray)
     return image_gray 
 
 
-img_gris= convert_gray(img)
-img_gris_array = np.array(img_gris)
+Element=np.array(['C','Ca','Fe','Mn','Na','O','P','S','Si','Ti','V'])
+for el in Element:
+    txt=f"25-projet-data-processing\projet_mines-Paris-data-processing_2026\donnes_MEB-EDS\export_tif\Laitier1_{el}-Kα.tif"
+   
+    img = Image.open(txt)
+    img_gris= convert_gray(img)
+    img_gris_array = np.array(img_gris)
 
-h,l = np.shape(img_gris_array)
-vmin = np.min(img_gris_array)
-vmax = np.max(img_gris_array)
+    h,l = np.shape(img_gris_array)
+    vmin = np.min(img_gris_array)
+    vmax = np.max(img_gris_array)
+    print(el,img_gris_array)
 
-print("The size is :", (h,l),  ". The minimum value is :", vmin, ". The maximum value is:", vmax)
+    print("For", el, "The size is :", (h,l),  ". The minimum value is :", vmin, ". The maximum value is:", vmax)
+
+
+
+
+
